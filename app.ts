1b4150fe8cdcf19d49e7dde3371304cf9ccdcf3b
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-10-16 23:58:43 +0300

    check process.env.mongoURI

--- app.ts
+++ app.ts
@@ -8,5 +8,6 @@ const PORT = process.env.PORT || process.env.DEV_PORT;
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+console.log(process.env.mongoURI,"check mongo uri");
 Routes.getAllProductRoutes(app);
 app.listen(PORT);
--- model/db.ts
+++ model/db.ts
@@ -1,5 +1,6 @@
 
 const mongoose = require("mongoose");
+console.log(process.env.mongoURI,"from db");
 mongoose.connect(process.env.mongoURI)
 .then()
 .catch(err=>console.log(err,"cheking error type"))

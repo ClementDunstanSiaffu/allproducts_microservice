ac17219bbf8bd5407f8d3c3d4d215dfbb74d49b1
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-10-17 00:22:43 +0300

    remove all console.log

--- model/db.ts
+++ model/db.ts
@@ -1,8 +1,7 @@
 
 const mongoose = require("mongoose");
-console.log(process.env.mongoURI,"from db");
 mongoose.connect(process.env.mongoURI)
 .then()
-.catch(err=>console.log(err,"cheking error type"))
+.catch(err=>false)
 
 require("./schema")

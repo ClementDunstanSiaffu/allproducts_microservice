5389f048f7714dcd5e4804730a9ec241b8423920
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2022-10-16 21:50:03 +0300

    adding console.log in mongodb connection

--- model/db.ts
+++ model/db.ts
@@ -1,5 +1,7 @@
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.mongoURI);
+mongoose.connect(process.env.mongoURI)
+.then()
+.catch(err=>console.log(err,"cheking error type"))
 
-require("./schema");
+require("./schema")
